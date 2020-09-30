d6874a1f59b16f530c988c299db2c2bfeadd45ad
phutsadeejandee <phutsadee.ja.61@ubu.ac.th>
2020-09-30 14:28:43 +0700

    Updates

---
+++ precache-manifest.c99e067c8ed382cc5e9a9b59727ee5fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50792d46efdb8d794871fa24ed1f26a",
+    "url": "/commath-tictactoe/index.html"
+  },
+  {
+    "revision": "1413bc2b25f6464e9157",
+    "url": "/commath-tictactoe/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "df804eab6bda7c3e89a5",
+    "url": "/commath-tictactoe/static/js/2.0938f15a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/commath-tictactoe/static/js/2.0938f15a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1413bc2b25f6464e9157",
+    "url": "/commath-tictactoe/static/js/main.f3928f16.chunk.js"
+  },
+  {
+    "revision": "249ab273bcb10bbe080d",
+    "url": "/commath-tictactoe/static/js/runtime-main.b3df6b71.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/commath-tictactoe/static/media/logo.5d5d9eef.svg"
+  }
+]);
